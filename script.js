6ff67ea5c98b6cc287105062aef8c55ae71e596a
Hyperion <86941111+hhyperion1009@users.noreply.github.com>
2025-10-19 22:13:32 +0700

    Thêm câu chúc và sửa giao diện

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const users = {
     },
     "Mai Khanh": {
         pass: "11072008",
-        msg: "",
+        msg: "20/10 tới rồi!!!! Nhận được lời chúc của tớ là một đặc quyền mà chỉ cậu có được thôi đó!! Tớ xin chúc cậu phải luôn thành công trên con đường mình chọn, ngày càng xinh đẹp, tươi tắn và kiêu sa như hoa hồng. Đặc biệt là cậu hãy trở thành một hoa hướng dương luôn hướng về phía mặt trời nha!! Cậu đừng có tự ti về bản thân mình nữa vì nhất định cậu sẽ làm được. Believe in yourself ❤",
         music: "MKhanh.mp3"
     },
     "Minh Anh": {
         pass: "05072008",
-        msg: "",
+        msg: "Đến specture có spec chờ Thật là một niềm vinh hạnh tột bậc khi các bá tước chúng tôi được hiện diện nơi đây - giữa khung cảnh hoa lệ của lớp 12CH - để chúc mừng ngày trọng đại 20/10 của quý cô Phan de Minh d’Anh. Xin gửi đến quý cô kiêu sa lời chúc rạng ngời nhất: luôn tươi tắn như điểm mười thể dục, dịu dàng như bài kiểm tra dễ (trừ các bài cô thấy khó), và đặc biệt là tiếp tục tỏa sáng như đèn sân khấu mỗi khi giáo viên gọi tên! Chúc quý cô một ngày 20/10 ngập tràn niềm vui, quà tặng và những lời khen tựa ánh rượu vang chan chứa tình cảm từ các vị bá tước nơi đây. Lời cuối, cầu chúc cho quý cô sẽ sớm nhận được lời mời nhập học từ ngôi trường trong mộng nơi xứ sở hoa tulip xa xôi, để rồi một ngày không xa, cả trời Âu cũng phải ngả mũ trước trí tuệ và phong thái thanh lịch của người con gái Việt Nam ấy. 🗣️🗣️",
         music: "MAnh.mp3"
     },
     "Trâm Anh": {
@@ -21,7 +21,7 @@ const users = {
     },
     "Hà Minh": {
         pass: "15072008",
-        msg: "",
+        msg: "Halo hola quát súp haminh!! Hôm nay là ngày phụ nữ Việt Nam, tập thể xã đoàn 12CH xin gửi đến những lời chúc và những cái ôm online đầy chân thành đến môm - một lớp phó học tập kiên cường chịu đựng bị random trúng tên + làm việc một mình trong 3 năm qua 💥❗💯. Chúc cho himonk sẽ có 1 ngày, 1 năm, 10 năm, 100 năm đầy vui vẻ và hạnh phúc, có nhìu thành công trong tương lai nhó hihi 😋. Tụi mình biết là haminh có rất nhiều ước mơ trong những năm đại học sắp tới, làm bác sĩ “thý u” hay làm nhà chiêm tinh học,... nữa. Thì tụi tui mong là haminh dù có đi theo con đường nào, cũng sẽ đạt được những thành quả xứng đáng với công sức của bản thân, với làm gì làm cũng phải chọn ngành mà mình thiệt đam mê và thấy nó thú vị nhaaaa 🐱👀 phải cook mấy năm đại học của mình thành món “kiến thức đại học tái lăn” nha bà nọi. Hãy tỏa sáng thật nhiệt trong chặng đường tương lai nhe, tụi tui tin nó sẽ được trải bằng ✨15072008✨ lần trúng gacha =)))",
         music: "HMinh.mp3"
     },
     "Việt Hà": {
@@ -30,8 +30,8 @@ const users = {
         music: "VHa.mp3"
     },
     "Thị Tòng": {
-        pass: "0",
-        msg: "",
+        pass: "CH2326",
+        msg: "Hihi tụi con chào cô Tòng xinh đẹp diệu kỳ tiên tử giáng trần huyền thoại hóa học ạ 😜🥳 Hôm nay là ngày phụ nữ Việt Nam, tập thể xã đoàn 12CH xin được gửi đến cô những lời chúc và tình iu ngập tràn đến cô ạaaa 💕💘 Cô còn nhớ lần tụi con giải thích cho cô về ý nghĩa biệt danh lớp 12CH hong, lúc trước nó dịch ra là “xe hốt rác” (hơi mùi một chút), nhưng sau một khoảng thời gian học với cô, từ những tiết trong giờ cho tới tiết đội tuyển thứ 7, tụi con còn hốt thêm được siu nhìu kiến thức + kinh nghiệm làm bài từ cô nữa !!! Tụi con tin là tất cả sẽ giúp tụi con làm bài thật tốt trong kì thi sắp tới 💟💫 Tụi con chúc cô sẽ mãi cống hiến hết mình trong việc đào tạo được thật nhiều lứa học sinh chuyên Hóa đầy nhiệt huyết và tài năng trong thời gian sắp tới. Chúc cô mãi tươi trẻ và sống khỏe, ăn siêu ngon, ngủ siêu ngon, vì ăn được ngủ được thì mới là tiên nữ xinh đẹp được í ạ hihihi 🧚‍♀️💅",
         music: ""
     }
 }
